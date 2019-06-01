ca2957d9ea4410eaa92d878e49324350b2aba64d
Fabien Culot <fab.culot@gmail.com>
2019-06-01 12:42:32 +0200

    init plugin file

---
+++ testimonials_slider_wp.php
@@ -0,0 +1,9 @@
+<?php
+/*
+Plugin Name: Slider testimonial ESGI
+Plugin URI: http://slider-testimonial-esgi.com
+Description: Un plugin pour intégrer un slider et un formulaire pour les témoignages
+Author: Alix, Charles, Fabien
+Author URI: http://nous.com
+Version: 0.1
+*/
