9ad4f7e6c0263d5b80e8ca0319b24ddbd6a6e66c
Fabien Culot <fab.culot@gmail.com>
2019-06-21 13:02:11 +0200

    update plugin init

--- PluginInit.php
+++ PluginInit.php
@@ -5,19 +5,20 @@ class PluginInit
     {
       add_shortcode('testimonial_plugin', 'create_slider');
       function testimonial_init(){
-        return create_slider();
+        return create_slider($atts, $content);
       }
 
 
-    function create_slider(){
-      // $atts = shortcode_atts(array('numberposts' => 5), $atts);
-      // $html = array();
-      // $html[] = $content;
-      // $html[] =
-      //
-      // echo implode('', $html);
-      return  '<p>Test</p>';
+    function create_slider($atts, $content){
+      $atts = shortcode_atts(array('numberposts' => 5), $atts);
+      $html = array();
+      $html[] = $content;
+      $html[] ='<div id="carouselExampleFade" class="carousel slide carousel-fade" data-ride="carousel">';
+      $html[] .='<p>Test slider</p>';
+      $html[] .='</div>';
 
+
+      echo implode('', $html);
     }
 
     function create_form(){
