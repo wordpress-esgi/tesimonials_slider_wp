b74e0a98b3f30726d32b14e880bf59ef6e172f36
Fabien Culot <fab.culot@gmail.com>
2019-07-01 15:23:40 +0200

    Fix slider testimonial

--- PluginInit.php
+++ PluginInit.php
@@ -7,9 +7,10 @@ class PluginInit
       function testimonial_init($atts, $content= null) {
           $a = shortcode_atts(
               array(
-                  'numberposts' => '4',
-                  'parametre2' => '4000',
-                  'parametre3'=> null
+                  'numberslide' => '2',
+                  'speed' => '4000',
+                  'prev'=> 'chevron-left',
+                  'next'=> 'chevron-right'
               ), $atts);
           //transforme les paramètres en variables
 //          extract($atts);
@@ -22,17 +23,20 @@ class PluginInit
 
       $testimonials = fetchTestimonialApproved();
 
-      $numberPosts = $atts['numberposts'];
-      $param2= $atts['parametre2'];
-      $param3= $atts['parametre3'];
+      $numberSlide = $atts['numberslide'];
+      $speed= $atts['speed'];
+      $prev = $atts['prev'];
+      $next = $atts['next'];
+
       $html = '';
       $html .='<div class="container">';
       $html .=' <div class="row">';
       $html .='   <div class="col-md-12">';
-      $html .='     <div id="carouselTestimonial" class="carousel slide" data-ride="carousel" data-interval="'.$param2.'">';
+      $html .='     <div id="carouselTestimonial" class="carousel slide" data-ride="carousel" data-interval="'.$speed.'">';
       $html .='        <div class="carousel-inner text-center carousel-testimonial-plugin">';
       $i = 0;
       foreach($testimonials as $testimonial){
+        if($i < $numberSlide){
           if ($i == 0) {
             $html .='         <div class="carousel-item active slide-testimonial-plugin">';
             $html .='               <p class="testimonial-message">'.$testimonial->message.'</p>';
@@ -44,13 +48,14 @@ class PluginInit
             $html .='               <p class="testimonial-user"><small>'.$testimonial->user_name.'</small></p>';
             $html .='         </div>';
           }
+        }
         $i++;
       }
       $html .='       </div>';
       $html .='     </div>';
 
-      $html .='<a class="carousel-control-prev testimonial-control" href="#carouselTestimonial" role="button" data-slide="prev"><i class="fa fa-chevron-left"></i></a>';
-      $html .='<a class="carousel-control-next testimonial-control" href="#carouselTestimonial" role="button" data-slide="next"><i class="fa fa-chevron-right"></i></a>';
+      $html .='<a class="carousel-control-prev testimonial-control" href="#carouselTestimonial" role="button" data-slide="prev"><i class="fas fa-'.$prev.'"></i></a>';
+      $html .='<a class="carousel-control-next testimonial-control" href="#carouselTestimonial" role="button" data-slide="next"><i class="fas fa-'.$next.'"></i></a>';
       $html .='   </div>';
       $html .=' </div>';
       $html .='</div>';
